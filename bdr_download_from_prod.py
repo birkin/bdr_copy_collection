7777bcd5338fc507ac4a808acc989c7576876aa7
Birkin James Diana <birkin.diana@gmail.com>
2023-08-09 08:50:49 -0400

    builds out outline.

--- bdr_download_from_prod.py
+++ bdr_download_from_prod.py
@@ -18,7 +18,6 @@ logging.basicConfig(
     format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s', 
     datefmt='%d/%b/%Y %H:%M:%S' )
 log = logging.getLogger(__name__)
-log.debug( 'log set' )
 
 
 def run_manager():
@@ -30,7 +29,12 @@ def run_manager():
     ## end def run_manager()
 
 
-## helper functions -------------------------------------------------
+class Runner():
+
+    def __init__(self) -> None:
+        self.pid = ''
+        self.collection_pid = ''
+        self.level = ''
 
 
 ## dudermain --------------------------------------------------------
@@ -39,3 +43,17 @@ if __name__ == '__main__':
     log.debug( '\n\nstarting dundermain...' )
     run_manager()
     log.debug( 'processing complete' )
+
+
+parser = argparse.ArgumentParser(description='Downloads items from the production BDR to be copied onto the dev instance (using a companion script in a separate step).')
+parser.add_argument("-p", "--pid", help="Provide a BDR pid to download that item and all necessary connections (parent/child/etc).", type=str)
+parser.add_argument("-c", "--collection", help="Provide a BDR collection pid to download all items in that collection (BE CAREFUL! Some collections are extremely large!).", type=str)
+parser.add_argument("-l", "--level", default='thumbnail', help="Specifies what resolution of images to download, or metadata only. Options are 'metadata', 'thumbnail' (default), or 'full'.", type=str)
+args = parser.parse_args()
+collection_to_grab = args.collection
+if collection_to_grab:
+    print('This mode is not yet implemented.')
+    sys.exit(0)
+pid_to_grab = args.pid
+print(pid_to_grab)
+level = args.level
