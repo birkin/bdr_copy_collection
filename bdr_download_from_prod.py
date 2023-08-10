d8831f914643ed776f71fed59873a1703a326117
Birkin James Diana <birkin.diana@gmail.com>
2023-08-10 08:28:17 -0400

    refactors argument-handling.

--- bdr_download_from_prod.py
+++ bdr_download_from_prod.py
@@ -37,57 +37,50 @@ class Runner():
         self.level = ''
 
 
-## dudermain --------------------------------------------------------
-
-# if __name__ == '__main__':
-#     log.debug( '\n\nstarting dundermain...' )
-#     run_manager()
-#     log.debug( 'processing complete' )
-
-
-# parser = argparse.ArgumentParser(description='Downloads items from the production BDR to be copied onto the dev instance (using a companion script in a separate step).')
-# parser.add_argument("-p", "--pid", help="Provide a BDR pid to download that item and all necessary connections (parent/child/etc).", type=str)
-# parser.add_argument("-c", "--collection", help="Provide a BDR collection pid to download all items in that collection (BE CAREFUL! Some collections are extremely large!).", type=str)
-# parser.add_argument("-l", "--level", default='thumbnail', help="Specifies what resolution of images to download, or metadata only. Options are 'metadata', 'thumbnail' (default), or 'full'.", type=str)
-# args = parser.parse_args()
-# collection_to_grab = args.collection
-# if collection_to_grab:
-#     print('This mode is not yet implemented.')
-#     sys.exit(0)
-# pid_to_grab = args.pid
-# print(pid_to_grab)
-# level = args.level
 
 
 
-# -------------------------------------------------------------------
-# caller handlers
-# -------------------------------------------------------------------
+## caller handlers --------------------------------------------------
 
-
-def parse_args() -> dict:
+def parse_args() -> tuple:
     """ Parses arguments when module called via __main__ """
-    parser = argparse.ArgumentParser( description='Downloads items from the production BDR to be copied onto the dev instance (using a companion script in a separate step).' )
-    parser.add_argument( '--pid', '-p', help='Provide a BDR pid to download that item and all necessary connections (parent/child/etc).', type=str )
-    parser.add_argument( '--collection', '-c', help='Provide a BDR collection pid to download all items in that collection (BE CAREFUL! Some collections are extremely large!).', type=str )
-    parser.add_argument( '--level', '-l', default='thumbnail', help='Specifies what resolution of images to download, or metadata only. Options are "metadata", "thumbnail" (default), or "full".', type=str )
+    parser = argparse.ArgumentParser( description='Downloads items from the production BDR to be copied onto the dev instance (using a companion script in a separate step). Note: either a `pid` or `collection` pid are required.' )
+    parser.add_argument( '-p', '--pid', help='BDR item-pid; will download that item and all necessary connections (parent/child/etc).', type=str )
+    parser.add_argument( '-c', '--collection_pid', help='BDR collection-pid; will download all items in that collection (BE CAREFUL! Some collections are extremely large!).', type=str )
+    parser.add_argument( '-l', '--level', default='thumbnail', help='Specifies what resolution of images to download, or metadata only. Options are "metadata", "thumbnail" (default), or "full".', type=str )
     args: dict = vars( parser.parse_args() )
-    log.debug( f'args, ```{pprint.pformat(args)}```' )
-    return args
+    log.debug( f'args, ```{args}```' )
+    assert type(args) == dict, type(args)
+    assert type(parser) == argparse.ArgumentParser, type(parser) 
+    return ( parser, args )
 
-def validate_args( args ) -> None:
+def validate_args( args ) -> bool:
     """ Validates arguments when module called via __main__ """
-    if not args['pid'] and not args['collection']:
-        err_msg = 'no pid or collection-pid specified; quitting'
+    if args['pid'] and args['collection_pid']:
+        err_msg = '\nonly one of `pid` or `collection_pid` should be specified; quitting\n'
         log.error( err_msg )
         print( err_msg )
-        sys.exit( 0 )
-    pass
+        valid = False
+    elif not args['pid'] and not args['collection_pid']:
+        err_msg = '\none of `pid` or `collection_pid` must be specified; quitting\n'
+        log.error( err_msg )
+        print( err_msg )
+        valid = False
+    else:
+        valid = True
+    log.debug( f'valid, `{valid}`' )
+    assert type(valid) == bool, type(valid)
+    return valid
+
+## dudermain --------------------------------------------------------
 
 if __name__ == '__main__':
-    args: dict = parse_args()
-    args_valid = validate_args( args )
+    ( parser, args ) = parse_args()
+    args_valid: bool = validate_args( args )
+    if not args_valid:
+        parser.print_help()
+        sys.exit( 0 )
     pid: str = args['pid']
-    collection: str = args['collection']
+    collection: str = args['collection_pid']
     level: str = args['level']
     run_manager( pid, collection, level )
